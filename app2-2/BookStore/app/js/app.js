1c366e51417eea0ce48662da7f6d4509c136ef55
baojian <252532242@qq.com>
2016-11-17 20:29:00 +0800

    路由和MVC

---
+++ app2-2/BookStore/app/js/app.js
@@ -0,0 +1,16 @@
+(function(angular) {
+	"user strict"
+    var bookStoreApp = angular.module('bookStoreApp', ['ngRoute', 'bookStoreCtrls']);
+
+    bookStoreApp.config(function($routeProvider) {
+        $routeProvider.when('/hello', {
+            templateUrl: 'templs/hello.html',
+            controller: 'HelloController'
+        }).when('/bookList', {
+            templateUrl: 'templs/bookList.html',
+            controller: 'BookListController'
+        }).otherwise({
+            redirectTo: '/hello'
+        })
+    })
+})(window.angular)
---
+++ app2-2/BookStore/app/js/controllers.js
@@ -0,0 +1,21 @@
+(function(angular) {
+	"user strict";
+    var bookStoreCtrls = angular.module('bookStoreCtrls', []);
+
+    bookStoreCtrls.controller('HelloController', function($scope) {
+        $scope.love = 'love';
+    });
+
+    bookStoreCtrls.controller('BookListController', function($scope) {
+        $scope.bookList = [{
+            title: 'HTML',
+            name: 'HTML5实战'
+        }, {
+            title: 'JS',
+            name: 'javascript权威指南'
+        }, {
+            title: 'angularJS',
+            name: 'AngularJS从做项目开始'
+        }];
+    });
+})(window.angular)
